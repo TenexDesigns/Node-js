e6bbaad48432cef8636a503bf60d2c515360c301
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-05-25 20:58:17 +0300

    Create How_to_run_node.js

---
+++ How_to_run_node.js
@@ -0,0 +1,43 @@
+To run javascript using node, there are two ways of doing this
+
+
+1. Node REPL -Read Evaluate Print Loop
+   To use this just type ----> "node" in the terminal and an interactive shell where you can write code comes up 
+
+
+  e.g 
+PS D:\node> node 
+Welcome to Node.js v18.12.1.      
+Type ".help" for more information.
+> console.log("Hello george Gacau");
+Hello george Gacau
+undefined
+>
+
+
+2. Excuting code in a javascrip file in the commandline
+
+e.g 
+PS D:\node> node index.js
+Hello George Gacau
+ PS D:\node> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

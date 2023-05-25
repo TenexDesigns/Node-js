21f03ef5b28a80c48a82309224ff103aacc74fb3
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-05-25 20:45:52 +0300

    Update Node_1.js

--- Node_1.js
+++ Node_1.js
@@ -3,15 +3,37 @@ Node js is a runtime enviroment for excuting javascript outside of the broser
 Ryan dahi introduced an amzing concept called Node in 2008. - Excuting javascript programs outside of the brosser on any machine
 
 
+Javascript run time - Is an enviromet which provides all the necessaruy components in order too use and run a javascript program
 
 
 
+WHAT IS NODE JS
+
+Node js is an open source crosspaltform javascript run-time envroment
+
+it is
+
+Open sorce
+Crossplatform - runs on limux, mac and windows
+Javascript runtime enviroment - Provides all necessary components in order to use and run a javascript program outside the broser.
 
 
 
+---> It is not a languag ,it is not a framwork
+---> It is capable of excuting javascript outside a broser
+---> It can not only  excute the standrd javascript language, but also new  features that are made available through c++ bindings , using the v* engine
+
+WHAT CAN YOU BUULD WITH NODE JS
+
+Traditional websites
+Backend services like APIs
+Real-time applications
+Streaming servies
+ClI tools
+Multiplayer games
 
 
-
+Node js allows you to build complex and powerful applications
 
 
 

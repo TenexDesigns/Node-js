e343f5a6a9de806b3d3d4800b23660f9f6c06cf5
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-05-26 08:41:49 +0300

    Create Built_in_module.js

---
+++ Built_in_module.js
@@ -0,0 +1,117 @@
+Types of Modules
+
+
+Local Modules
+Built in modules
+Third party modules
+
+
+
+
+
+BUILT IN MODULES
+
+These are modules that node.js ships with
+ They are also refered to as core modulesYou first have to  import these modules before you can use them
+ 
+ 
+ Some of the Built in module we are going to discuss indclue
+ 
+         Path
+         Events
+         Fs
+         stream
+         http
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

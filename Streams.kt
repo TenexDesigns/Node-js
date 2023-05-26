06b5c0022921c338990ee936d10970fee22a24e0
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-05-26 17:28:40 +0300

    Update Streams.kt

--- Streams.kt
+++ Streams.kt
@@ -18,8 +18,39 @@ We rarelu use strreams directly
 Other modules use streams for their functioning.
 
 
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+Streams are a fundamental concept in Node.js, representing asynchronous flow of data.
+They are used to handle reading and writing files or exchanging information efficiently.
+There are four types of streams in Node.js: readable, writable, duplex, and transform streams medium.com, section.io.
 
+Readable streams: 
+These streams can read data from a particular data source, such as a file system or user input 
+via stdin in a terminal application. They emit events like 'data' and 'end' when data is available or when the stream
+is finished blog.appsignal.com.
 
+Writable streams:
+These streams can write data to a destination. They emit events like 'data', 'end', and 'error' when data
+is written or when an error occurs section.io.
+
+Duplex streams: 
+These streams can be used for both reading and writing operations. They can be used in scenarios like
+a TCP server that needs to handle incoming and outgoing data section.io.
+
+Transform streams: 
+These streams are a type of duplex stream where the output is computed based on the input. 
+They are commonly used for data manipulation, compression, and decompression section.io.
+
+Streams are useful in various scenarios, such as handling large files, exchanging data between client and server,
+and processing data in chunks, which helps reduce memory usage and improve performance blog.appsignal.com.
+However, its important to note that using streams can increase the complexity of your application,
+so make sure the benefits outweigh the added complexity blog.appsignal.com.
+ 
+ 
+ 
+ 
+ 
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ 
 
 
 const fs = require("node:fs");
@@ -107,14 +138,88 @@ const writeableStream = fs.createWriteStream("./file2.txt")
 
 readablestream.pipe(writeableStream)  // This will write the data in file.txtx to file2.txt
 
+A pipe retunrs a destination stream whicl allows chaning. The condintio for this is that The destination stream has to be readable,duplex  or a transform stream
+The destination stream has to be readable,duplex  or a transform stream
+
+
+In our above stream, readablestream.pipe(writeableStream), we have writeable stream , so we can not chain it by calling writeable.pipe
+
+Let us change the implemention for above and use another build in module called zlib, for compressin files and turning them into zip files
+
+CHAINGING WITH THE PIPE METHOD
+
+const fs = require("node:fs");
+const zlib = require('node:zlib')  // Provideds compressin functionality implemented by the gzlip algorithm
+//zlib allows us to creat zip files
+//zlib has the built in transform stream
+
+
+const gzip = zlib.createGzip()
+
+
+
+// This will enable us to read data in chucks fro file.txt
+const readableStream = fs.createReadStream("./file.txt",{
+    encoding :'utf-8',
+})
+
+
+readableStream.pipe(gzip)//This returns a transform stream so we can call .pipe and pass in fs.writestream('./destiantion to write the fie')
+readableStream.pipe(gzip).pipe(fs.WriteStream('./file2.txt.gz'))  
+// We are chaing with .pipe
+//moving from readable stream to transform stram to a writeable stream
+
+
+
+
+
+
 
 
 
 CHAINING PIPES
 
 
+Pipes in Node.js are used to connect multiple streams together, allowing data to flow from one stream to another.
+This is particularly useful for transferring data between files, compressing and decompressing files,
+and implementing inter-process communication between Node.js and other languages like Python.
+The pipe method is the recommended way to consume streams, as it abstracts away the need to handle events and provides a clean and efficient way to pass data between streams guru99.com, javatpoint.com, mariokandut.com, freecodecamp.org.
 
+Heres an example of piping and chaining streams using the pipe method:
 
+const fs = require('fs');
+const zlib = require('zlib');
+
+const input = fs.createReadStream('input.txt');
+const output = fs.createWriteStream('input.txt.gz');
+
+const compressor = zlib.createGzip();
+const decompressor = zlib.createGunzip();
+
+input.pipe(compressor).pipe(output);
+
+output.on('finish', () => {
+  console.log('Compression complete.');
+});
+In this example, the input.txt file is compressed using the zlib library and the compressed data is written to 
+a new file named input.txt.gz. The pipe method is used to connect the input stream to the compressor stream and 
+then to the output stream. The finish event is emitted when the compression is complete,
+and the decompression can be done similarly javatpoint.com.
+
+You can also chain multiple pipe calls together, as shown in this example:
+
+readableSrc
+  .pipe(transformStream1)
+  .pipe(transformStream2)
+  .pipe(finalWritableDest);
+This is equivalent to chaining the streams together using the | operator in a Unix-like shell freecodecamp.org.
+
+Its important to note that the pipe method should be used with streams that implement a Readable interface,
+and the destination stream must implement a Writable interface. You can chain multiple pipe calls together
+for more complex data manipulation and processing mariokandut.com, freecodecamp.org.
+
+Using the pipe method is beneficial because it allows you to handle data in smaller chunks,
+preventing memory issues and maintaining good performance in your applications. The code is shorter, elegant, and easy to follow
 
 
 

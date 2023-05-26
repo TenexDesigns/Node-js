a9adc1b98dfbc0dbf3fad99216d960fa55480310
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-05-26 08:19:14 +0300

    Create Watch_mode_in_node.js

---
+++ Watch_mode_in_node.js
@@ -0,0 +1,37 @@
+Thiis helps restart node and detect chage made when you save a file
+
+
+index.js
+
+console.log("Hello George Gacau ")
+
+
+
+
+
+
+
+PS D:\node> node --watch node.js
+
+
+
+th result
+PS D:\node> node --watch node.js
+(node:3680) ExperimentalWarning: Watch mode is an experimental feature. This feature could change at any time
+(Use `node --trace-warnings ...` to show where the warning was created)
+Hello George 
+Completed running 'node.js'
+
+
+
+
+
+
+
+
+
+
+
+
+
+

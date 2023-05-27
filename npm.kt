26aaf091ba4738a6f5ae3ae4ffe68141127f60df
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-05-27 15:44:56 +0300

    Create npm.kt

---
+++ npm.kt
@@ -0,0 +1,196 @@
+What is NPM
+
+Npm - Is the worlds largest software library  (registry)
+It is a software package manager
+
+
+
+1. NPM is a Software Library
+If you think of a library, - It contains  books written by diiferen authores
+A book library contains books written by diffrent authores
+
+
+Simialry   - Npm is a libry or registry which contains packages written by various developers
+
+
+It is a large public database of javascript code tht developers from all over the world can use to share and borrow code.
+If you author a "code packaage", you can publish it to the npm registry for others to use
+
+On the other hand, if you come across a code package that is authored by someone else and solves the problem you have at hand, you can  brooorw that code without having to reinvent the wheel
+
+
+
+
+2. Npm is a software package Manage
+But --->
+  
+how does a developer publish a package to the registry
+
+how does a developer consume a package from the registry
+
+what happens if the package author decides to change a function name in a package
+
+how would a consumer of that package update an already installed package
+
+what if the package I am consuming is dependent on another package
+
+
+----------------
+as you can see managing packages in a project is not as simple as it sounds
+
+
+this is where npm as a package manager comes to the rescue ,
+we have access to a command line interface tool that lets us install and manage packages in a project
+
+
+
+
+
+CONCEPTS IN THE NPM UMBRELLLA
+
+Package.json
+
+What it is.
+
+Package.json is npms configuration file
+
+It is a json file that typicaly lives in the root directory of your package and holds various metadat relevant to th package.
+
+
+Why we need it
+
+Package.json is the central place to configure and describe how to interact with and run your package
+
+It is primary used by the npm CLI
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+....

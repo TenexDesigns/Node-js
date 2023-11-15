354f032bf0ca55ecd3aec5e5a557e747027972c5
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-15 22:20:19 +0300

    Create Learn Node 2.js

---
+++ Leran Node/Learn Node 2.js
@@ -0,0 +1,27 @@
+This article provides a comprehensive guide on the best way to learn Node.js. It emphasizes the importance of understanding JavaScript thoroughly as a prerequisite for learning Node.js.
+
+**Key Points Discussed:**
+
+1. **JavaScript Proficiency:** A strong foundation in JavaScript is essential before embarking on Node.js learning. This includes understanding concepts like Promises, generators, rest/spread operators, destructuring, Proxy/Reflect, Arrow functions, classes, and more.
+
+2. **Node.js as JavaScript Extension:** Node.js is essentially JavaScript with a different set of libraries compared to browser-based JavaScript. It lacks browser APIs and relies on stdout, stdin, and console functions for user interaction.
+
+3. **Node.js Libraries and Resources:** Node.js offers a vast collection of libraries for various functionalities, including file system access, networking, encryption, and more. These libraries are well-documented and can be found alongside NPM modules.
+
+4. **Learning through Code Reading and Writing:** The most effective way to learn Node.js is by actively reading and writing code. This involves exploring existing modules, understanding their organization, and experimenting with different approaches.
+
+5. **Avoiding Frameworks Initially:** While frameworks can be helpful later, it's advisable to avoid them during the initial learning phase. Frameworks can mask underlying functionalities, hindering a deeper understanding of Node.js concepts.
+
+6. **IDE and Bash Shell Skills:** Using a good IDE, such as WebStorm, can enhance productivity and code organization. Basic Bash shell commands and scripting knowledge can be beneficial for interacting with the command line.
+
+7. **Networking Concepts and Single-Threaded Nature:** Understanding basic networking protocols and concepts is helpful for building network-based applications using Node.js. It's important to remember that Node.js is single-threaded and utilizes libuv for event scheduling, which is not pre-emptive.
+
+8. **Independent Learning and Knowledge-Based Choices:** Avoid getting swayed by programming ideologies or debates. Develop your own programming style based on knowledge and experimentation.
+
+9. **Seeking Help and Perseverance:** Don't hesitate to seek help when encountering difficulties. Numerous resources, including Quora, are available for assistance. Remember to make the effort to solve problems independently before seeking external aid.
+
+10. **Linux or Mac Environment:** Working on Linux or Mac is recommended for a more seamless Node.js development experience. If using Windows, install Bash on Ubuntu on Windows to provide a native Linux/bash environment.
+
+11. **Continuous Code Reading and Writing:** Consistently reading and writing code is the most effective way to solidify understanding and gain proficiency in Node.js and JavaScript.
+
+In summary, the article highlights the importance of a strong JavaScript foundation, practical code exploration, and independent learning to effectively master Node.js.

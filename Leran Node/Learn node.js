eb55590a4cae22ca455fec15c6ebabe8ee81e63c
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-15 22:19:29 +0300

    Create Learn node.js

---
+++ Leran Node/Learn node.js
@@ -0,0 +1,51 @@
+The article provides practical advice on learning Node.js, assuming the reader already knows how to program. Here are the key points highlighted about learning Node.js:
+
+1. **Prerequisites:**
+   - Learn to program if not already familiar with programming basics.
+
+2. **Learn JavaScript Thoroughly:**
+   - Understand all aspects of the JavaScript language, including features like Promises, generators, rest/spread, destructuring, Proxy/Reflect, Arrow functions, and classes.
+
+3. **Node.js is JavaScript with Different Libraries:**
+   - Node.js is essentially JavaScript with different libraries than those used in the browser.
+   - It lacks browser APIs, and output/input is done using stdout, stdin, or their console function wrappers.
+
+4. **Node.js Libraries:**
+   - Node.js provides libraries for file system access, network interface, encryption, etc.
+   - The official Node.js documentation and npm (Node Package Manager) are valuable resources for learning and exploring modules.
+
+5. **Learning Approach:**
+   - Books and courses are useful for a general idea, but the best way to learn is by a combination of writing code and reading other people's code.
+
+6. **Practical Learning Steps:**
+   - Start by reading and understanding existing modules on npm.
+   - Observe how modules are organized, check their package.json files, and understand Node.js project structure.
+   - Learning involves a continuous process of reading and writing code.
+
+7. **JavaScript and Node.js Flexibility:**
+   - Node.js is considered easy due to minimal requirements to get started and no stringent requirements to become proficient.
+   - JavaScript's speed and flexibility allow developers to write code in a style that suits them.
+
+8. **Single Language for Front-end and Back-end:**
+   - With JavaScript and Node.js, one language serves for both front-end and back-end development.
+   - Node.js offers a unified ecosystem.
+
+9. **Key Learning Steps (Assuming JavaScript Knowledge):**
+   - Read code and write code in parallel.
+   - Avoid frameworks while learning.
+   - Use a good Integrated Development Environment (IDE) like WebStorm.
+   - Understand basic bash shell commands and scripting.
+   - Know basic networking protocols.
+
+10. **Additional Tips:**
+    - JavaScript is single-threaded and runs to completion.
+    - Ignore JavaScript ideologies; find your own coding style.
+    - Don't get discouraged; seek help when needed.
+    - Consider working on Linux or Mac for a better development environment.
+
+In summary, the article emphasizes the hands-on approach of reading and writing code as the most effective way to learn Node.js. It encourages practical exploration of existing modules, project structures, and continuous learning. The provided tips cover a range of aspects, from coding style to the development environment, to support a comprehensive learning experience.
+
+
+
+
+  ....
